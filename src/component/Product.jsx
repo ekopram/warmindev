21490a4ce1b32046986790b5d7748489b75332a7
ekopram <ekopram9@yahoo.co.id>
2024-07-07 00:59:20 +0700

    add: ui "search, category"

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -5,10 +5,12 @@ const Product = ({ menu, handleAddProduct }) => {
   // console.log({ handleClick });
 
   return (
-    <div className="w-52 h-56 p-1 flex flex-col items-center">
-      <img className="w-32 h-32" src={menu.image} alt="" />
-      <h1>{menu.name}</h1>
-      <div className="flex justify-between w-4/5 mt-12">
+    <>
+      <div className="w-48 h-48 p-1 flex flex-col items-center">
+        <img className="w-36 h-36" src={menu.image} alt="" />
+        <h1>{menu.name}</h1>
+      </div>
+      <div className="flex justify-center gap-3 w-full">
         <h1 className="p-1 font-bold">Rp.{menu.price}</h1>
         <button
           onClick={() => handleAddProduct(menu)}
@@ -17,7 +19,7 @@ const Product = ({ menu, handleAddProduct }) => {
           Add
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Product from "../component/Product";
 import Slider from "../component/Slider";
+import { FaSearch } from "react-icons/fa";
 
 const Menu = ({ handleAddProduct, topMenus, menus }) => {
   const listTopMenus = topMenus.map((menu) => (
-    <div key={menu.id} className="card bg-white shadow-md p-2 w-56 h-64 ">
+    <div key={menu.id} className="card bg-white drop-shadow-xl p-2 w-52 h-64 ">
       <Product menu={menu} handleAddProduct={handleAddProduct} />
     </div>
   ));
 
   const listMenus = menus.map((menu) => (
-    <div key={menu.id} className="card bg-white shadow-md p-2 w-56 h-64 ">
+    <div key={menu.id} className="card bg-white drop-shadow-xl p-2 w-52 h-64 ">
       <Product menu={menu} handleAddProduct={handleAddProduct} />
     </div>
   ));
@@ -27,6 +28,30 @@ const Menu = ({ handleAddProduct, topMenus, menus }) => {
       <section className="w-full ">
         <div className="py-10">
           <h1 className=" text-center text-3xl">MENU</h1>
+          <div className="mt-16 justify-center flex gap-2">
+            <input
+              className="drop-shadow-xl rounded-lg p-2"
+              placeholder="search"
+              type="search"
+            />
+            <button className="bg-white drop-shadow-xl px-4 rounded-lg text-xl hover:bg-blue-500 hover:text-white">
+              <FaSearch />
+            </button>
+          </div>
+          <div className="flex gap-5 justify-center mt-10">
+            <div className="bg-white rounded-full drop-shadow-xl py-1 px-4 hover:bg-black hover:text-white">
+              Food
+            </div>
+            <div className="bg-white rounded-full drop-shadow-xl py-1 px-4 hover:bg-black hover:text-white">
+              Drink
+            </div>
+            <div className="bg-white rounded-full drop-shadow-xl py-1 px-4 hover:bg-black hover:text-white">
+              Snack
+            </div>
+            <div className="bg-white rounded-full drop-shadow-xl py-1 px-4 hover:bg-black hover:text-white">
+              Desert
+            </div>
+          </div>
           <div className=" grid grid-cols-3 gap-5  mt-10 w-fit mr-auto ml-auto">
             {listMenus}
           </div>
