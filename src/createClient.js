5b0a0b7748d271061dec70c37e2fb95f13a51b28
ekopram <ekopram9@yahoo.co.id>
2024-07-19 23:41:11 +0700

    creating .env

--- src/createClient.js
+++ src/createClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://drkktokcafsrjqgruyff.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRya2t0b2tjYWZzcmpxZ3J1eWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwNTM1NjksImV4cCI6MjAzNjYyOTU2OX0.yKxCDmGPYLTGTvj5kEWlr4WQecpz3ZN1V0yaQp4_5YM"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
